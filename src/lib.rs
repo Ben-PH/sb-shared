61750364af6c8ee1a03c3a1b5bc0877574ebca2a
Ben-PH <benphawke@gmail.com>
2020-08-12 09:18:01 +0200

    Correct Login and User structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 use serde::{Deserialize, Serialize};
 
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Login {
+    pub email: String,
+    pub password: String,
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct User {
     pub first_name: String,
     pub last_name: String,
     pub email: String,
-    pub password: String,
 }
